1b680bac561588b37f9ca95190d49b365409ced5
paulsukow <paul.sukow@gmail.com>
2018-05-29 12:19:06 -0600

    Add buy now selectors tot he Borderlands2GamePage

--- src/test/groovy/geb/component/library/Borderlands2GamePage.groovy
+++ src/test/groovy/geb/component/library/Borderlands2GamePage.groovy
@@ -7,6 +7,7 @@ class Borderlands2GamePage extends Page {
    static at = { title == "Borderlands - Borderlands 2" }
 
    static content = {
-
+      buyNowMenuOption(wait: true) { $('li.hasdrop').$('a', text: iContains('buy now')) }
+      borderlands2SubMenuOption(wait: true) { $('li.hassubdrop').$('a', text: iContains('Borderlands 2')) }
    }
 }
