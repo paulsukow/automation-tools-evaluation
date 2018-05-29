19680113cee9e9b62f8ead2f30a082da7f9e7fb4
paulsukow <paul.sukow@gmail.com>
2018-05-29 10:53:16 -0600

    Add selectors for the date combo boxes and submit button on the AgeVerificationPage page object

--- src/test/groovy/geb/component/library/AgeVerificationPage.groovy
+++ src/test/groovy/geb/component/library/AgeVerificationPage.groovy
@@ -10,6 +10,10 @@ class AgeVerificationPage extends Page {
 
    static content = {
 
-      birthMonth(wait: true) { $('div[name="birthMonth:"]') }
+      birthMonth(wait: true) { $('select', name: 'birthmonth:') }
+      birthDay(wait: true) { $('select', name: 'birthday') }
+      birthYear(wait: true) { $('select', name: 'birthyear') }
+
+      submitButton(wait: true) { $('input', name: 'Submit') }
    }
 }
