32c77fd78e3e957f038722d83e0dba92be4e9f08
paulsukow <paul.sukow@gmail.com>
2018-05-29 10:54:21 -0600

    Add BorderlandsHomePage page object

---
+++ src/test/groovy/geb/component/library/BorderlandsHomePage.groovy
@@ -0,0 +1,12 @@
+package geb.component.library
+
+import geb.Page
+
+class BorderlandsHomePage extends Page {
+
+   static at = { title == "Borderlands - Home Page" }
+
+   static content = {
+
+   }
+}
