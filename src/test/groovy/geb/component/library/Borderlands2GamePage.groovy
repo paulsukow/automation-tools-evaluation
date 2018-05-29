b3565c85e90179a532185759e12db7508a99e060
paulsukow <paul.sukow@gmail.com>
2018-05-29 11:26:19 -0600

    Add Borderlands2GamePage page object

---
+++ src/test/groovy/geb/component/library/Borderlands2GamePage.groovy
@@ -0,0 +1,12 @@
+package geb.component.library
+
+import geb.Page
+
+class Borderlands2GamePage extends Page {
+
+   static at = { title == "Borderlands - Borderlands 2" }
+
+   static content = {
+
+   }
+}
