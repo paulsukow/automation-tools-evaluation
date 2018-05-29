fb0b943b0ee8a6141158b7b7643aa31df01d551a
paulsukow <paul.sukow@gmail.com>
2018-05-29 09:48:11 -0600

    Set up Geb evaluation with yo geb

---
+++ src/test/groovy/geb-evaluation/Config.groovy
@@ -0,0 +1,20 @@
+package geb-evaluation
+
+class Config {
+   private static def CONFIG_LOCATION = 'geb-evaluation/Profiles.groovy'
+
+   public static final def CONFIG = parseConfig().profiles[profile()]
+
+   Config() {
+
+   }
+
+   private static def parseConfig() {
+      def configFile = new Config().getClass().getClassLoader().getResource(CONFIG_LOCATION).toExternalForm().toURI().toURL()
+      new ConfigSlurper().parse(configFile)
+   }
+
+   private static def profile() {
+      System.getProperty("profile", "production")
+   }
+}
---
+++ src/test/groovy/geb-evaluation/tests/LandingPageTest.groovy
@@ -0,0 +1,17 @@
+package geb-evaluation.tests
+
+import geb-evaluation.component.library.LandingPage
+import geb.spock.GebReportingSpec
+import spock.lang.Stepwise
+
+@Stepwise
+class LandingPageTest extends GebReportingSpec {
+   def "should be able to launch Google"() {
+      when: "you open Google"
+      to LandingPage
+
+      then: "you are at google"
+      at LandingPage
+      search.present
+   }
+}
