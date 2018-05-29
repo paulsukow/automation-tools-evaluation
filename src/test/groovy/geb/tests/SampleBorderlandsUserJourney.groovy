99b88b240400e1d3f97b4117809b1ab58e70bbc2
paulsukow <paul.sukow@gmail.com>
2018-05-29 11:27:42 -0600

    Add steps to navigate to the borderlands 2 game page to the SampleBorderlandsUserJourney

--- src/test/groovy/geb/tests/SampleBorderlandsUserJourney.groovy
+++ src/test/groovy/geb/tests/SampleBorderlandsUserJourney.groovy
@@ -1,9 +1,13 @@
 package geb.tests
 
 import geb.component.library.AgeVerificationPage
+import geb.component.library.Borderlands2GamePage
 import geb.component.library.BorderlandsHomePage
 import geb.spock.GebReportingSpec
+import org.openqa.selenium.interactions.Actions
+import spock.lang.Stepwise
 
+@Stepwise
 class SampleBorderlandsUserJourney extends GebReportingSpec {
 
    def "should be able to reach the borderlands website by completing the age verification"() {
@@ -20,7 +24,21 @@ class SampleBorderlandsUserJourney extends GebReportingSpec {
 
       ageVerificationPage.submitButton.click()
 
-      then: "you are redirected the the borderlands home page"
+      then: "you are redirected to the borderlands home page"
       browser.at(BorderlandsHomePage)
+   }
+
+   def "should be able to navigate to the borderlands 2 game page"() {
+      given: "you are at the borderlands home page"
+      BorderlandsHomePage borderlandsHomePage = browser.at(BorderlandsHomePage)
+
+      when: "you select the borderlands 2 game from the games menu"
+      interact {
+         moveToElement(borderlandsHomePage.gamesMenuOption)
+      }
+      borderlandsHomePage.borderlands2SubMenuOption.click()
+
+      then: "you are redirected to the borderlands 2 game page"
+      browser.at(Borderlands2GamePage)
    }
 }
