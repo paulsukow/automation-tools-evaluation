8e444f5d6252d8bdcec83fa7933ebc139082b5c2
paulsukow <paul.sukow@gmail.com>
2018-05-29 11:27:08 -0600

    Add selectors for the games menu and the borderlands 2 game option in the games menu to the BorderlandsHomePage

--- src/test/groovy/geb/component/library/BorderlandsHomePage.groovy
+++ src/test/groovy/geb/component/library/BorderlandsHomePage.groovy
@@ -7,6 +7,7 @@ class BorderlandsHomePage extends Page {
    static at = { title == "Borderlands - Home Page" }
 
    static content = {
-
+      gamesMenuOption(wait: true) { $('li.hasdrop').$('a', text: iContains('games')) }
+      borderlands2SubMenuOption(wait: true) { $('li.hassubdrop').$('a', text: iContains('Borderlands 2')) }
    }
 }
